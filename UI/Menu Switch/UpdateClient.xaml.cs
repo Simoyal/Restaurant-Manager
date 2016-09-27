1cc9f57062647cc376a1418a7a60040c59a72c4b
Simoyal <simonmoyal@wireless-47-174.jct.ac.il>
2016-09-27 23:56:26 +0300

    The purpose of the project was to create a system for restaurant to manage branches, dish list, order list, and business statistics (top branch or most rentable dish).
    The project is finish, need to change some part of the UI

---
+++ UI/Menu Switch/UpdateClient.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BE;
+using BL;
+namespace UI.Menu_Switch
+{
+    /// <summary>
+    /// Logique d'interaction pour UpdateClient.xaml
+    /// </summary>
+    public partial class UpdateClient : UserControl, iSwitchable
+    {
+        Client client;
+        IBL bl;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public UpdateClient()
+        {
+            InitializeComponent();
+            client = new Client();
+            this.DataContext = client;
+            bl = Factory_BL.GetBL();
+            clientNameTextBox.IsEnabled = ageTextBox.IsEnabled = addressTextBox.IsEnabled = addressForDeliveryTextBox.IsEnabled = numberTelephoneTextBox.IsEnabled = creditCardNumberTextBox.IsEnabled = false;
+            ClientCombobox.ItemsSource = bl.ListOfClient();
+        }
+        /// <summary>
+        /// Unlocking all fields
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClientCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ClientCombobox.SelectedValue != null)
+            {
+                clientNameTextBox.IsEnabled = ageTextBox.IsEnabled = addressTextBox.IsEnabled = addressForDeliveryTextBox.IsEnabled = numberTelephoneTextBox.IsEnabled = creditCardNumberTextBox.IsEnabled = true;
+
+            }
+        }
+        /// <summary>
+        /// Update Client information
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (bl.isEligibedForOrder(client))
+                {
+                    bl.UpdateClient(client);
+                    client = new Client();
+                    this.DataContext = client;
+                    Switcher.Switch(new UpdateMenuOrderWindow());
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MainOrder());
+        }
+
+        private void clientNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // if we succefully convert the text of the textbox it means that we only have number which is forbidden
+            int tmp;
+            if (int.TryParse(clientNameTextBox.Text, out tmp))
+            {
+                clientNameTextBox.Text = "";
+            }
+        }
+
+        private void ageTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // if we have some trouble to convert the text of the textbox it means that we have non number character which is forbidden
+            int tmp;
+            if (!int.TryParse(ageTextBox.Text, out tmp))
+            {
+                ageTextBox.Text = "0";
+            }
+        }
+
+        private void numberTelephoneTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // if we have some trouble to convert the text of the textbox it means that we have non number character which is forbidden
+            int tmp;
+            if (!int.TryParse(numberTelephoneTextBox.Text, out tmp))
+            {
+                numberTelephoneTextBox.Text = "0";
+            }
+        }
+
+        private void creditCardNumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // if we have some trouble to convert the text of the textbox it means that we have non number character which is forbidden
+            int tmp;
+            if (!int.TryParse(creditCardNumberTextBox.Text, out tmp))
+            {
+                creditCardNumberTextBox.Text = "0";
+            }
+        }
+    }
+}
---
+++ UI/NumericQuantity.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Logique d'interaction pour NumericQuantity.xaml
+    /// </summary>
+    public partial class NumericQuantity : UserControl
+    {
+        private float? num = null;
+        public float? Value
+        {
+            get { return num; }
+            set
+            {
+
+
+                if (value > MaxValue)
+                    num = MaxValue;
+                else if (value < MinValue)
+                    num = MinValue;
+                else
+                    num = value;
+
+                txtNum.Text = num == null ? "" : num.ToString();
+            }
+        }
+        public int MinValue { get; set; }
+
+
+        public int MaxValue
+        {
+            get { return (int)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MaxValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxValueProperty =
+            DependencyProperty.Register("MaxValue", typeof(int), typeof(NumericQuantity), new PropertyMetadata(100));
+        public NumericQuantity()
+        {
+            InitializeComponent();
+        }
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null || txtNum.Text == "-")
+                return;
+
+            float val;
+            if (!float.TryParse(txtNum.Text, out val))
+                txtNum.Text = Value.ToString();
+            else
+                Value = val;
+        }
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            Value++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            Value--;
+        }
+    }
+}
